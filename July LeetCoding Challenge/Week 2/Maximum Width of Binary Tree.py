bced5e01cf81008c23b0114f8cd44055f4c7fbdf
Shivakumar98 <36780562+Shivakumar98@users.noreply.github.com>
2020-07-13 09:28:22 +0530

    Create Maximum Width of Binary Tree.py

---
+++ July LeetCoding Challenge/Week 2/Maximum Width of Binary Tree.py
@@ -0,0 +1,84 @@
+Given a binary tree, write a function to get the maximum width of the given tree. The width of a tree is the maximum width among all levels. The binary tree has the same structure as a full binary tree, but some nodes are null.
+
+The width of one level is defined as the length between the end-nodes (the leftmost and right most non-null nodes in the level, where the null nodes between the end-nodes are also counted into the length calculation.
+
+Example 1:
+
+Input: 
+
+           1
+         /   \
+        3     2
+       / \     \  
+      5   3     9 
+
+Output: 4
+Explanation: The maximum width existing in the third level with the length 4 (5,3,null,9).
+
+Example 2:
+
+Input: 
+
+          1
+         /  
+        3    
+       / \       
+      5   3     
+
+Output: 2
+Explanation: The maximum width existing in the third level with the length 2 (5,3).
+
+Example 3:
+
+Input: 
+
+          1
+         / \
+        3   2 
+       /        
+      5      
+
+Output: 2
+Explanation: The maximum width existing in the second level with the length 2 (3,2).
+
+Example 4:
+
+Input: 
+
+          1
+         / \
+        3   2
+       /     \  
+      5       9 
+     /         \
+    6           7
+Output: 8
+Explanation:The maximum width existing in the fourth level with the length 8 (6,null,null,null,null,null,null,7).
+
+
+Note: Answer will in the range of 32-bit signed integer.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def widthOfBinaryTree(self, root: TreeNode) -> int:
+        q=deque([(root,1)])
+        w=0
+        while q:
+            na,left=q[0]
+            na,right=q[-1]
+            w=max(w,right-left+1)
+            next=deque()
+            while q:
+                node,i=q.popleft()
+                if node.left: 
+                    next.append((node.left,i*2))
+                if node.right: 
+                    next.append((node.right,i*2+1))
+            q=next
+             
+        return w
